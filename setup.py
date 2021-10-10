2b6a465b4321d8bcfcbea410c56ae60e2703a8f2
devfemibadmus <devfemibadmus@gmail.com>
2021-10-10 13:31:23 +0100

    devfemibadmus

---
+++ setup.py
@@ -0,0 +1,7 @@
+import os
+
+os.system("pip install pip --upgrade pip")
+os.system("apt upgrade && pkg upgrade")
+os.system("pip install django")
+os.system("pip install pillow")
+os.system("pip install psycopg")
