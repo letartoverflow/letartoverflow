256d80897df042f6efaecb96235378d20f399e7b
devfemibadmus <devfemibadmus@gmail.com>
2022-04-26 21:57:35 +0100

    update

--- stop.py
+++
@@ -1,6 +0,0 @@
-import os
-
-try:
-   os.system("pg_ctl -D $PREFIX/var/lib/postgresql stop")
-except:
-   print("setUpError")
--- studio/urls.py
+++
@@ -1,8 +0,0 @@
-from . import views
-from django.urls import path, include
-
-urlpatterns = [
-    path('', views.home, name='Home'),
-    path('post', views.post, name='Post'),
-    path('<int:id>/edit', views.edit_post, name='Edit Post')
-]
